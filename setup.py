72028d1f84a4f4f044f5b7b83a2156cc93fc38a9
yogesh <yogeshkhangode.99@gmail.com>
2022-08-28 01:24:40 +0530

    src pipeline

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Yogi776",
+    description="A small package for dvc ml pipeline demo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Yogi776/dvc-mlops",
+    author_email="yogeshkhangode.99@gmail.com",
+    packages=["src"],
+    python_requires=">=3.7",
+    install_requires=[
+        'dvc',
+        'pandas',
+        'scikit-learn'
+    ]
+)
---
+++ src/utils/all_utils.py
@@ -0,0 +1,15 @@
+import yaml
+import os
+import src
+
+def read_yaml(path_to_yaml:str) -> dict:
+    with open(path_to_yaml) as yaml_file:
+        content = yaml.safe_load(yaml_file)
+    return content
+
+
+
+def create_directory(dirs: list):
+    for dir_path in dirs:
+        os.makedirs(dir_path,exist_ok = True)
+        print(f"directory is created at {dir_path}")
