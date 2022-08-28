ad690f1eda7cbc4c1dac9c2c102a28afc1798325
yogesh <yogeshkhangode.99@gmail.com>
2022-08-28 10:22:47 +0530

    dvc update

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -1,15 +1,16 @@
 import yaml
 import os
-import src
+import json
 
-def read_yaml(path_to_yaml:str) -> dict:
+
+def read_yaml(path_to_yaml: str) -> dict:
     with open(path_to_yaml) as yaml_file:
         content = yaml.safe_load(yaml_file)
-    return content
 
+    return content
 
 
 def create_directory(dirs: list):
     for dir_path in dirs:
-        os.makedirs(dir_path,exist_ok = True)
+        os.makedirs(dir_path, exist_ok=True)
         print(f"directory is created at {dir_path}")
