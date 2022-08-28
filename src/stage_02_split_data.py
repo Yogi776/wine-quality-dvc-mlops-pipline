46cfb737266fad34b4d84eb33a796e5976963025
yogesh <yogeshkhangode.99@gmail.com>
2022-08-28 11:56:09 +0530

    stage_02split

--- src/stage_02_split_data.py
+++ src/stage_02_split_data.py
@@ -1,11 +1,14 @@
-from src.utils.all_utils import read_yaml, create_directory
+from distutils.command.config import config
+from src.utils.all_utils import read_yaml, create_directory, save_local_df
 import argparse
+from sklearn.model_selection import train_test_split
 import pandas as pd
 import os
 
-def get_data(config_path):
+
+def split_and_save(config_path,params_path):
     config = read_yaml(config_path)
-    
+    params = read_yaml(params_path)
     
 
     artifacts_dir = config["artifacts"]['artifacts_dir']
@@ -19,7 +22,27 @@ def get_data(config_path):
     raw_local_file_path = os.path.join(raw_local_dir_path, raw_local_file)
     
     df = pd.read_csv(raw_local_file_path)
-    print(df)
+
+    split_ratio = params["base"]["test_size"]
+    random_state = params["base"]["random_state"]
+    
+    train,test = train_test_split(df,test_size = split_ratio,random_state = random_state)
+    
+    split_data_dir = config["artifacts"]["split_data_dir"]
+
+    create_directory([os.path.join(artifacts_dir,split_data_dir)])
+
+
+    train_data_filename = config["artifacts"]["train"]  
+    test_data_filename = config["artifacts"]["test"]  
+
+
+    train_data_path = os.path.join(artifacts_dir,split_data_dir, train_data_filename)
+
+    test_data_path = os.path.join(artifacts_dir,split_data_dir, test_data_filename)
+
+    for data, data_path in (train,train_data_path), (test,test_data_path):
+        save_local_df(data,data_path)
 
 
 
@@ -29,8 +52,11 @@ if __name__ == '__main__':
 
     args.add_argument("--config", "-c", default="config/config.yaml")
 
+    args.add_argument("--params", "-p", default="params.yaml")
+
     parsed_args = args.parse_args()
 
-    get_data(config_path=parsed_args.config)
+
+    split_and_save(config_path=parsed_args.config,params_path = parsed_args.params)
 
 
