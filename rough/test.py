07205c0e9f31fe21bd7642959a7ade4809335930
yogesh <yogeshkhangode.99@gmail.com>
2022-08-28 09:34:49 +0530

    stage 1

--- rough/test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def main():
-    print(">>>> ALWAYS RUN >>>>>>")
-    with open(os.path.join("rough", "test.txt"), "w") as f:
-        f.write("This is a test file!!")
-
-if __name__ == '__main__':
-    main()
